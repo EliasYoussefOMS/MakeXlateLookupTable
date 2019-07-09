1b5b11954396491d98679227b7c64f2cbb98f26a
Elias <Elias@DESKTOP-HM1DDIQ>
2019-07-09 15:19:24 +0200

    removes slashes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class lookupTableItem {
 
 	getLine() {
 		let line;
-		line = "/" + this.left + "/" + this.right + "/";
+		line = "/" + this.left + "/" + this.right.replace("/", " ") + "/";
 		return line;
 	}
 }
